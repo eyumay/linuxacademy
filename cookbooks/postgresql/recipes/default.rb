76bde48af78e75a2bfd79dc144c28f037a293fb7
Eyuel Wolderufael <eyuel.gessese@gmail.com>
2017-03-07 02:26:08 +0000

    Adding postgresql cookbook

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 package 'postgresql-server' do 
-	notifies :run, 'execute[postgresql-init]'
+	notifies :run, 'execute[postgresql-init]' # -> this is doing: it sends run signal to postgresql-init to run. 
 end
 
 execute 'postgresql-init' do 
@@ -13,6 +13,7 @@ execute 'postgresql-init' do
 	action :nothing
 end
 
+# execute the below to enable service, and then start it. 
 service 'postgresql' do 
 	action [:enable, :start]
 end
