3bdd749fc067e2c3fbc59e53083a0650ebe9b42a
Eyuel Wolderufael <exw3uoy@c02xr34cjg5mmbp.dv9119.homedepot.com>
2019-08-12 08:16:38 -0400

    add Packer tester

---
+++ ci/scripts/launch_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e -u -x
+
+# mv dependency-cache/node_modules hello_hapi
+# cd hello_hapi && npm test
+cd ~/Documents/HomeDepot/repos1/packerLocalBuild/packer-example
+packer build packer.json
